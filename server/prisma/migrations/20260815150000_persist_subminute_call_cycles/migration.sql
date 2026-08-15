e5d119232791656a900f8e5ef4cb8de981030603
projetoever <projetoever.green@gmail.com>
2026-08-15 14:30:43 -0300

    fix: target mapped call table in timer migration

--- server/prisma/migrations/20260815150000_persist_subminute_call_cycles/migration.sql
+++ server/prisma/migrations/20260815150000_persist_subminute_call_cycles/migration.sql
@@ -1,5 +1,5 @@
 -- Preserve sub-minute attendance and follow-up cycles instead of rounding them to zero.
-ALTER TABLE "AndonCall"
+ALTER TABLE "andon_calls"
   ALTER COLUMN "attendanceMinutes" TYPE DOUBLE PRECISION
     USING "attendanceMinutes"::DOUBLE PRECISION,
   ALTER COLUMN "postMaintenanceMinutes" TYPE DOUBLE PRECISION
